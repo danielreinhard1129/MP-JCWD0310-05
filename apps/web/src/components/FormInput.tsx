43cf03c0e27ca0644c7ccb170f4f36d840bd12aa
upilupal <naufalhanif891@gmail.com>
2024-05-08 02:55:54 +0700

    fix: resolve conflict

--- apps/web/src/components/FormInput.tsx
+++ apps/web/src/components/FormInput.tsx
@@ -1,14 +1,16 @@
-
 'use client';
+import React from 'react';
+
+import { HTMLInputTypeAttribute } from 'react';
+import { FormikHandlers } from 'formik';
 
 import { Input } from './ui/input';
 import { Label } from './ui/label';
-import { FormikHandlers } from 'formik';
 
 interface FormInputProps {
   name: string;
   label: string;
-  type: string;
+  type: string | HTMLInputTypeAttribute;
   value: string;
   placeholder: string;
   isError: boolean;
@@ -16,6 +18,7 @@ interface FormInputProps {
   handleChange: FormikHandlers['handleChange'];
   handleBlur: FormikHandlers['handleBlur'];
 }
+
 
 const FormInput: React.FC<FormInputProps> = ({
   name,
@@ -48,66 +51,5 @@ const FormInput: React.FC<FormInputProps> = ({
 };
 
 export default FormInput;
-=======
-"use client"
-import React from 'react'
-import { Label } from '../components/ui/label'
-import { Input } from '../components/ui/input'
-import { HTMLInputTypeAttribute } from 'react'
-import { FormikHandlers } from 'formik'
 
-interface FormInputProps {
-    name: string;
-    placeholder: string;
-    label: string;
-    type: HTMLInputTypeAttribute;
-    value: string;
-    isError: boolean;
-    error: string | undefined;
-    handleChange: FormikHandlers["handleChange"];
-    handleBlur: FormikHandlers["handleBlur"];
-}
-
-const FormInput: React.FC<FormInputProps> = ({
-    name,
-    placeholder,
-    label,
-    type = "text",
-    handleChange,
-    handleBlur,
-    value,
-    isError,
-    error,
-}) => {
-    
-  return (
-    <div className="flex flex-col space-y-1.5">
-                  <Label
-                    htmlFor={name}
-                    className={
-                      isError
-                        ? "text-red-500"
-                        : ""
-                    }
-                  >
-                    {label}
-                  </Label>
-                  <Input
-                    name={name}
-                    placeholder={placeholder}
-                    type={type}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={value}
-                  />
-                  {isError ? (
-                    <div className="text-xs text-red-500">
-                      {error}
-                    </div>
-                  ) : null}
-                </div>
-  )
-}
-
-export default FormInput
 
