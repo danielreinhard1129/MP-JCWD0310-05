feba5a6d927765d2be7ceaac082f0ceaa552a1f2
RyandhiRouqo <158122500+RyandhiRouqo@users.noreply.github.com>
2024-05-11 01:41:11 +0700

    feat: register02 (#7)

---
+++ apps/web/src/app/forgot-password/page.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { useFormik } from 'formik';
+import FormInput from '@/components/FormInput';
+import { Button } from '@/components/ui/button';
+import { validationSchema } from './validationSchema';
+import useForgotPassword from '@/hooks/api/auth/useForgotPassword';
+import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+const ForgotPassword = () => {
+  const { forgotPassword, isLoading } = useForgotPassword();
+  const router = useRouter();
+  const { values, errors, touched, handleSubmit, handleChange, handleBlur } =
+    useFormik({
+      initialValues: {
+        email: '',
+      },
+      validationSchema,
+      onSubmit: ({ email }) => {
+        forgotPassword(email);
+      },
+    });
+  return (
+    <main className="container mx-auto h-[90vh] px-4">
+      <div className="mt-40 flex justify-center">
+        <Card className="w-[450px]">
+          <CardHeader>
+            <CardTitle className="text-center text-3xl text-primary">
+              LOGO
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <div className="grid w-full items-center gap-4">
+                <FormInput
+                  name="email"
+                  type="email"
+                  label="Email"
+                  placeholder="Email"
+                  value={values.email}
+                  error={values.email}
+                  isError={!!touched.email && !!errors.email}
+                  handleChange={handleChange}
+                  handleBlur={handleBlur}
+                />
+              </div>
+              <Button className="mt-6 w-full" disabled={isLoading}>
+                {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                Forgot Password
+              </Button>
+              <div className='flex justify-center gap-1 py-4'>
+                <p
+                  className="text-center text-xs"
+                >
+                  Don't have an account?                   
+                </p>
+                <p 
+                className="cursor-pointer font-medium text-purple-700 text-xs"
+                    onClick={() => router.push('/register')}>
+                      Sign up
+                </p>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </main>
+  );
+};
+
+export default ForgotPassword;
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,18 +2,20 @@ import type { Metadata } from 'next';
 import { Inter, Poppins } from 'next/font/google';
 import './globals.css';
 import Navbar from "@/components/Navbar"
-import {Footer} from '@/components/Footer';
+import { Footer } from '@/components/Footer';
+import StoreProvider from '@/providers/StoreProvider';
 
 const poppins = Poppins({
-   subsets: ['latin'],
-   weight: ['400', '500', '600', '700'],
-   variable: '--font-poppins' });
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins'
+});
 
 export const metadata: Metadata = {
   title: 'Eventura',
   description: 'Event management website',
   icons: {
-    icon: '/public/vercel.svg' //kenapa gamau????
+    icon: 'vercel.svg' //kenapa gamau????
   }
 };
 
@@ -25,9 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Navbar />
-        {children}
-        
+        <StoreProvider>
+          <Navbar />
+          {children}
+
+        </StoreProvider>
       </body>
     </html>
   );
